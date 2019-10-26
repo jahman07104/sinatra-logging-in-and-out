927d2e100f71ae1c71c06d9f7c4025703e4eedc0
Patrick Harrison <jahman07104@gmail.com>
2019-10-26 12:04:57 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+  def self.current_user(session_hash)
+    @user=User.find_by_id(session_hash[:user_id])
+    
+    #user_id.save
+    #self.current_user
+  end
+
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
+
 end
